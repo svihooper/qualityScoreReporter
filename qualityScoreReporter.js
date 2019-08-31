f44623188c7a8ae16de83f07ce40f9b9c9d3861b
Jacob Schwarz <41166436+sv-jschwarz@users.noreply.github.com>
2019-08-30 17:07:58 -0700

    Update qualityScoreReporter.js

--- qualityScoreReporter.js
+++ qualityScoreReporter.js
@@ -2,18 +2,12 @@
  * Quality Score Listings Reporter
  * 
  * Used to generate a quick report of quality score totals for listings.
+ *
+ * Reference (https://github.com/sv-jschwarz/qualityScoreReporter) for instructions.
  * 
  * @version 1.0
  * @author sv-cmswo
  * @date 05/03/2019
- **/
-
-/**
- * Instructions:
- * 1. Create new Google spreadsheet
- * 2. Update "sheetId" below with your sheet id
- * 3. Share sheet with this user: 743184255003-2tbvikqou0c38ruhrmddno3kc7hnsmfr@developer.gserviceaccount.com
- * 4. Toggle "useTestConfig" to determine which config to use.
  **/
 
 var googleapisLib = require('@sv/googleapisLib');
